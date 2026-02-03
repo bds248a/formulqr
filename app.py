242583eff9ce2119bf1b54a94a353423ab9cb74c
bds248a <bds24.8a@gmail.com>
2026-02-03 13:47:41 +0200

    Add Geography Quiz using Streamlit

---
+++ app.py
@@ -0,0 +1,65 @@
+import streamlit as st
+
+st.title("🌍 Geography Quiz")
+
+score = 0
+
+
+st.write("1. What is the capital of Bulgaria?")
+q1 = st.text_input("Your answer (Q1):")
+if q1:
+    if q1.lower() == "sofia":
+        score += 1
+        st.write("Correct")
+    else:
+        st.write("Wrong")
+
+
+st.write("2. Which continent is Germany in?")
+q2 = st.text_input("Your answer (Q2):")
+if q2:
+    if q2.lower() == "europe":
+        score += 1
+        st.write("Correct")
+    else:
+        st.write("Wrong")
+
+
+st.write("3. What is the largest ocean?")
+q3 = st.text_input("Your answer (Q3):")
+if q3:
+    if q3.lower() == "pacific":
+        score += 1
+        st.write("Correct")
+    else:
+        st.write("Wrong")
+
+
+st.write("4. Which desert is the largest in the world?")
+q4 = st.text_input("Your answer (Q4):")
+if q4:
+    if q4.lower() == "sahara":
+        score += 1
+        st.write("Correct")
+    else:
+        st.write("Wrong")
+
+
+st.write("5. What is the longest river in the world?")
+q5 = st.text_input("Your answer (Q5):")
+if q5:
+    if q5.lower() == "nile":
+        score += 1
+        st.write("Correct")
+    else:
+        st.write("Wrong")
+
+
+if st.button("Show result"):
+    st.write("### Final score:")
+    st.write(score, "/ 5")
+
+    if score == 5:
+        st.write("🎉 Excellent!")
+    else:
+        st.write("Try again 🙂")
